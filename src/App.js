61d66d5f6ce096bb6105c671d9f518a8e00e23ca
Anastasia  Pankina <anastasiapankina@Anastasias-MacBook-Pro.local>
2024-04-24 12:44:57 +0800

    steps function implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handleNext() {
+    if (step < 3) setStep(step + 1);
+  }
+
+  function handlePrevious() {
+    if (step > 1) setStep(step - 1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
+      {isOpen && (
+        <Steps
+          step={step}
+          handlePrevious={handlePrevious}
+          handleNext={handleNext}
+        />
+      )}
+    </React.Fragment>
   );
 }
 
-export default App;
+function Steps({ step, handlePrevious, handleNext }) {
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
+      </div>
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+      <div className="buttons">
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={() => handlePrevious()}
+        >
+          Previous
+        </button>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={() => handleNext()}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
